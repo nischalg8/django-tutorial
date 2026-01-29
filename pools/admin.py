70ce1e1a853ea4f3531185f79f329bdaf3aec574
nischalg8 <nischalghimire99@gmail.com>
2026-01-30 00:24:22 +0545

    Configured admin panel and fixed adding media

--- pools/admin.py
+++ pools/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Questions, Choice
 # Register your models here.
 
-admin.site.register(Questions)
-admin.site.register(Choice)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 2  
+class QuestionAdmin(admin.ModelAdmin):
+     inlines = [ChoiceInLine]
+     
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'choice_text', 'votes')
+    
+admin.site.register(Questions, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
